d1ac30e762f93911c6c725e551374b348beb8b90
Neha Sanni <108986012+neha-ak0511@users.noreply.github.com>
2022-07-26 13:20:45 +0530

    Update dt-log.service.ts

--- src/app/services/dt-log.service.ts
+++ src/app/services/dt-log.service.ts
@@ -8,7 +8,7 @@ import { DtLog, LogSeverity } from '../model/dtlog-model';
 export class DtLogService {
 
   private dtLogEndpoint = "https://tyk57033.live.dynatrace.com/api/v2/logs/ingest";
-  private apiToken = "dt0c01.LYI7Y2XWN2KVVT3LGELADBCY.246BUOLXRWQZF4YHKEROSG6Y3AIF6PTNISIUPJROP3FP2V4QFIKAXEYFSBYUJTZR";
+  private apiToken = "";
   private headers = new HttpHeaders({
     'Content-Type': 'application/json; charset=utf-8',
     'Authorization': "Api-Token " + this.apiToken
